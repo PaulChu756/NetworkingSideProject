5227b7d6c60c4818fe4f1d22e660f978a94b7b82
Paul Chu <paul.chu@lafa.ad.aie.edu>
2016-05-19 10:48:38 -0500

    player added

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        float z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+        transform.Rotate(0, x, 0); // allows the user to rotate
+        transform.Translate(0, 0, z); // allows move forward/backwards.
+    }
+}
