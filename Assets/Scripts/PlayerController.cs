f28ecebd6ced119013613b5335d0c7d298f6c883
Paul Chu <paul.chu@lafa.ad.aie.edu>
2016-05-19 13:08:05 -0500

    School done.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,10 +1,16 @@
 ﻿using UnityEngine;
+using UnityEngine.Networking; // for network aware scripts
 using System.Collections;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : NetworkBehaviour // gameobjects that need to use network feature
 {
     void Update()
     {
+        if(!isLocalPlayer)
+        {
+            return;
+        }
+
         float x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
         float z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
 
