91470e1970fa9bb3689621411387b8648e420284
Paul Chu <lordbabypanda@gmail.com>
2016-05-20 15:15:43 -0500

    Missed up Player prefab.

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour
+{
+    public const int maxHealth = 100;
+    public int currentHealth = maxHealth;
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        if (currentHealth <= 0)
+        {
+            currentHealth = 0;
+            Debug.Log("I'm dead");
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,15 @@ using System.Collections;
 
 public class PlayerController : NetworkBehaviour // gameobjects that need to use network feature
 {
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+
+    /*
+     By setting the send rate to zero so the position is not synchronized over the network, 
+     and allowing each Client to calculate to position of the bullet, 
+     the overall network traffic is reduced to improve game performance.
+    */
+
     void Update()
     {
         if(!isLocalPlayer)
@@ -16,5 +25,31 @@ public class PlayerController : NetworkBehaviour // gameobjects that need to use
 
         transform.Rotate(0, x, 0); // allows the user to rotate
         transform.Translate(0, 0, z); // allows move forward/backwards.
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CmdFire();
+        }
+    }
+
+    [Command]
+    void CmdFire()
+    {
+        // Create Bullet
+        GameObject bullet = (GameObject)Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
+
+        // Add velocity to bullet
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 10;
+
+        // Spawn the bullet on the Clients
+        NetworkServer.Spawn(bullet); 
+
+        // Delete bullet after 2 seconds
+        Destroy(bullet, 2.0f);
+    }
+
+    public override void OnStartLocalPlayer() //override the orginal function, but still can change it.
+    {
+        GetComponent<MeshRenderer>().material.color = Color.yellow;
     }
 }
